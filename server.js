1057cd07db6a8689574316a979ebc585ecd1f8fa
Maulik Pipaliya <maulik.pipaliya@gmail.com>
2021-05-14 18:11:55 +0530

    Set up socket connection

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const socket = require('socket.io');
 
 const server = app.listen(3030, () => {
     console.log("Listening on 3030");
 });
 
 app.use(express.static('public'));
+
+
+//Socket Setup 
+
+
+const io = socket(server); 
+
+io.on('connection', (socket) => {
+    console.log("Made socket connection");
+    console.log(socket.id);
+})
